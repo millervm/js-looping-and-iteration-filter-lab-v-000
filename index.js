2f57484e832f55ee75fa97e55eb24015d91e9cec
Veronica Miller <vmcmiller@gmail.com>
2019-01-27 19:50:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function findMatching (drivers, name) {
   return drivers.filter(function (driver) {return driver.toLowerCase() == name.toLowerCase(); });
 }
+
+function fuzzyMatch (drivers, substring) {
+  return drivers.filter(function (driver) {return driver.toLowerCase().startsWith(substring.toLowerCase(); });
+}
